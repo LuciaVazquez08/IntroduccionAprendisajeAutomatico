a8d4c4336e9a2167ef7d7a5e618712f01e6f1e79
Lucia Vazquez <131535537+LuciaVazquez08@users.noreply.github.com>
2024-03-10 16:15:44 -0300

    clase 1

--- com/clase 1/test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-
-### LIBRERIAS PARA GRAFICAR
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-data = pd.read_csv('C://Users//Usuario//aprendizajeautomatico//IntroduccionAprendizajeAutomatico//res.csv', parse_dates=['UTC_date', 'onset_date'])
-data.head()
